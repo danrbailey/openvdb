89907137a5ab782cc1a61c33f3d578f282ab9ed4
Dan Bailey <danbailey@ilm.com>
2020-03-04 14:21:42 -0800

    Add Houdini CI caching workflow
    Signed-off-by: Dan Bailey <danbailey@ilm.com>

---
+++ ci/install_houdini2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+CACHING="$1"
+
+# move hou tarball into top-level
+cp hou/hou.tar.gz .
+tar -xzf hou.tar.gz
+
+# remove the hou directory, this disables the ability to cache
+if [ "$CACHING" == "OFF" ]; then
+    rm -rf hou
+fi
