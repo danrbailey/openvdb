467d905791c64844dba1f21e6c49423bd2f07b62
Dan Bailey <danbailey@ilm.com>
2020-05-23 14:40:19 -0700

    wip

--- ci/install_houdini2.sh
+++ ci/install_houdini2.sh
@@ -10,8 +10,3 @@ ls hou
 # move hou tarball into top-level
 cp hou/hou.tar.gz .
 tar -xzf hou.tar.gz
-
-# remove the hou directory, this disables the ability to cache
-if [ "$CACHING" == "OFF" ]; then
-    rm -rf hou
-fi
