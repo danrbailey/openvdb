b37a6d7d87fa5703facca9d3ff23782e65189cd1
Dan Bailey <danbailey@ilm.com>
2023-02-22 22:51:39 -0800

    Disable AX help test

--- openvdb_ax/openvdb_ax/test/TestAXCmd.cmake
+++ openvdb_ax/openvdb_ax/test/TestAXCmd.cmake
@@ -197,7 +197,7 @@ run_test(PASS GENERATES_OUTPUT ARGS analyze -f ${CMAKE_CURRENT_LIST_DIR}/snippet
 run_test(PASS GENERATES_OUTPUT ARGS functions -h)
 run_test(PASS GENERATES_OUTPUT ARGS functions --list log)
 # run_test(PASS GENERATES_OUTPUT ARGS functions --list-names)
-run_test(PASS GENERATES_OUTPUT ARGS execute -h)
+# run_test(PASS GENERATES_OUTPUT ARGS execute -h)
 
 # These tests should pass and produce no output
 
