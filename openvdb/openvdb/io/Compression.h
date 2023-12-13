ca1c237323cafb26481154a90e2a1f372aae328d
Dan Bailey <danbailey@ilm.com>
2023-12-12 20:08:22 -0800

    Revert "ILM: Apply a small change to io::writeCompressedValues() to fix an internal compiler error when using -fnon-call-exceptions"
    This reverts commit 325a2dfadda3300283e053e9cac9f26f8ccbc00f.

--- openvdb/openvdb/io/Compression.h
+++ openvdb/openvdb/io/Compression.h
@@ -649,6 +649,10 @@ writeCompressedValues(std::ostream& os, ValueT* srcBuf, Index srcCount,
     const uint32_t compress = getDataCompression(os);
     const bool maskCompress = compress & COMPRESS_ACTIVE_MASK;
 
+    Index tempCount = srcCount;
+    ValueT* tempBuf = srcBuf;
+    std::unique_ptr<ValueT[]> scopedTempBuf;
+
     int8_t metadata = NO_MASK_AND_ALL_VALS;
 
     if (!maskCompress) {
@@ -701,9 +705,8 @@ writeCompressedValues(std::ostream& os, ValueT* srcBuf, Index srcCount,
             /// are one of two values?
         } else {
             // Create a new array to hold just the active values.
-            Index tempCount = srcCount;
-            std::unique_ptr<ValueT[]> scopedTempBuf(new ValueT[srcCount]);
-            ValueT* tempBuf = scopedTempBuf.get();
+            scopedTempBuf.reset(new ValueT[srcCount]);
+            tempBuf = scopedTempBuf.get();
 
             if (metadata == NO_MASK_OR_INACTIVE_VALS ||
                 metadata == NO_MASK_AND_MINUS_BG ||
@@ -734,22 +737,14 @@ writeCompressedValues(std::ostream& os, ValueT* srcBuf, Index srcCount,
                 // Write out the mask that selects between two inactive values.
                 selectionMask.save(os);
             }
-
-            // Write out the buffer.
-            if (toHalf) {
-                HalfWriter<RealToHalf<ValueT>::isReal, ValueT>::write(os, tempBuf, tempCount, compress);
-            } else {
-                writeData(os, tempBuf, tempCount, compress);
-            }
-            return;
         }
     }
 
     // Write out the buffer.
     if (toHalf) {
-        HalfWriter<RealToHalf<ValueT>::isReal, ValueT>::write(os, srcBuf, srcCount, compress);
+        HalfWriter<RealToHalf<ValueT>::isReal, ValueT>::write(os, tempBuf, tempCount, compress);
     } else {
-        writeData(os, srcBuf, srcCount, compress);
+        writeData(os, tempBuf, tempCount, compress);
     }
 }
 
